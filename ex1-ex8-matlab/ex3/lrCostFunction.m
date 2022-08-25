902479d4a581506b9a95202cd79b1d6c54bc5805
Elmiraniz <96397248+Elmiraniz@users.noreply.github.com>
2022-08-25 16:34:51 -0400

    Finished part 1 of exercise 3

--- ex1-ex8-matlab/ex3/lrCostFunction.m
+++ ex1-ex8-matlab/ex3/lrCostFunction.m
@@ -36,7 +36,13 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+J = (sum(-y .* log(sigmoid(X * theta)) - (1-y).* log(1-sigmoid(X * theta))) + ...
+    lambda * sum(theta(2:end).^2) / 2 ) ./ m;
 
+% grad computation
+grad(1) = sum((sigmoid(X * theta) - y) .* X(:,1)) ./ m;
+temp = sigmoid(X * theta) - y;
+grad(2:end) = (X(:,2:end)' * temp + lambda .* theta(2:end)) ./ m;
 
 
 
